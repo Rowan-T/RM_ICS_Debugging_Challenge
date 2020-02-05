afa63befe6c63e1ff78aecfc7db7aa7c544ec54f
TheBestBee <eristic21@gmail.com>
2020-02-05 14:37:17 -0600

    Update Snyder_ICS_DC_01.py

--- ICS_Q3_Snyder_Debugging_Challenge/Snyder_ICS_DC_01.py
+++ ICS_Q3_Snyder_Debugging_Challenge/Snyder_ICS_DC_01.py
@@ -3,8 +3,8 @@
 # Rock, Paper, Scissors, Lizard, Spock.
 # Errors: ~6
 
- hands = ["R","P","S", #nothing to see here...
- ]
+ hands = ["R","P","S" #nothing to see here...
+         ]
 
 from random import randint as ran
 def position(List, Object):
@@ -12,14 +12,13 @@ def position(List, Object):
         if List(x) == Object:
             return x
 def RPS:
-    hands = ["R","P","S",
-]
+    hands = ["R","P","S"]
     Win = 0
     Los = 0
     Tie = 0
-    print("q to leave")
+    print("Q to leave")
     while True:
-        person_rps = input("Witch one? Rock Paper or Scissors(R/P/S)")
+        person_rps = input("Which one? Rock, Paper, or Scissors? (R/P/S)")
         if person_rps.upper() == "Q":
             break
         elif person_rps.upper() in hands:
@@ -34,8 +33,8 @@ def RPS:
             else:
                 Win += 1 
         else:
-            print("Cheaters loose")
-    print("Wins : %s, Losses: %s, Ties %s." % (Win Los,Tie))
+            print("Cheaters lose.")
+    print("Wins : %s, Losses: %s, Ties %s." % (Win, Los, Tie))
 
 def RPSLSp():
     hands = ["R","SP","P","L","S",]
@@ -44,8 +43,9 @@ def RPSLSp():
     Tie = 0
     print("q to leave")
     while True:
-        person_rps = input("Witch one? Rock Paper Scissors Lizard or Spock(R/P/S/L/SP)")
+        person_rps = input("Which one? Rock, Paper, Scissors, Lizard, or Spock? (R/P/S/L/SP)")
         if person_rps.upper() == "Q":
+        break
         elif person_rps.upper() in hands:
             computer_rps = str(hands[ran(1,5)-1])
             print computer_rps
