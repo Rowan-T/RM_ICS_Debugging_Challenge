0a404670c54949a10ba184b059ae057baafde73f
bschweighauser20 <60447838+bschweighauser20@users.noreply.github.com>
2020-02-07 14:52:14 -0600

    Add files via upload

---
+++ Debugging Challenge_2/DebuggingChallenge02.py
@@ -0,0 +1,7 @@
+x = 5
+y = 0
+# Ben Schweighauser
+# Runtime error: numeric value divided by zero
+# This would print out the product of the division problem
+# To fix the problem, set y to a numeric value that's not zero
+print(x/y)
